ad0ab888f08f05e7b898394477f6bedc2278134d
manojkumarlinux <mail@manojkumar.net>
2021-12-20 23:08:48 +0530

    error handled for config issues

--- src/Lib/Request.php
+++ src/Lib/Request.php
@@ -4,30 +4,31 @@ namespace SimpleApns\Lib;
 
 class Request {
 
-   
+    public static $code = [
+        '0'   => 'Config issues.'
+
+        '200' => 'Success.',
+
+        '400' => 'Bad request.',
+
+        '403' => 'There was an error with the certificate or with the provider’s authentication token.',
+
+        '404' => 'The request contained an invalid :path value.',
+
+        '405' => 'The request used an invalid :method value. Only POST requests are supported.',
+
+        '410' => 'The device token is no longer active for the topic.',
+
+        '413' => 'The notification payload was too large.',
+
+        '429' => 'The server received too many requests for the same device token.',
+
+        '500' => 'Internal server error.',
+
+        '503' => 'The server is shutting down and unavailable.',
+    ]; 
+
     public static function curlRequest(array $data) {
-
-        $responses = [
-            '200' => 'Success.',
-
-            '400' => 'Bad request.',
-
-            '403' => 'There was an error with the certificate or with the provider’s authentication token.',
-
-            '404' => 'The request contained an invalid :path value.',
-
-            '405' => 'The request used an invalid :method value. Only POST requests are supported.',
-
-            '410' => 'The device token is no longer active for the topic.',
-
-            '413' => 'The notification payload was too large.',
-
-            '429' => 'The server received too many requests for the same device token.',
-
-            '500' => 'Internal server error.',
-
-            '503' => 'The server is shutting down and unavailable.',
-        ];
 
         $url = $data['config']['url'].$data['deviceToken'];
 
@@ -37,13 +38,15 @@ class Request {
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
 
         curl_setopt($ch, CURLOPT_SSLCERT, $data['config']['keyPath']);
-        #curl_setopt($ch, CURLOPT_SSLCERTPASSWD, $data['config']['secretKey']);
+        curl_setopt($ch, CURLOPT_SSLCERTPASSWD, $data['config']['secretKey']);
 
         curl_setopt($ch, CURLOPT_FAILONERROR, true);
         curl_exec($ch);
         $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
-        return [ 'response' => $responses[$httpcode], 'code' => $httpcode];
+        $httpcode = isset(self::$responses[$httpcode]) ? $httpcode ? 0;
+
+        return [ 'response' =>  ? self::$responses[$httpcode], 'code' => $httpcode];
     } 
 }
